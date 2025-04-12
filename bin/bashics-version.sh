bc86b8bb626c56b196a0ae091bffc0aafe04133a
LES MATHESON <LMATHESON4@bloomberg.net>
2025-04-12 13:31:02 +0000

    Reconcile with template v2, add shfmt binary

--- bin/bashics-version.sh
+++ bin/bashics-version.sh
@@ -5,6 +5,14 @@
 KitVersion=0.9.2
 readonly KitVersion
 
+# The shellkit/ tooling naturally evolves out from under the dependent kits.  ShellkitSetupVers allows
+# detecting the need for refresh of templates/* derived files.  To bump the root version, 
+# zap all templates/* containing 'ShellkitTemplateVers' constants and changes to the corresponding dependent kits
+# Note that within templates/* there may be diverse versions in upstream shellkit, they don't all have to match,
+# but the derived copies should be sync'ed with upstream as needed.
+#shellcheck disable=2034
+ShellkitTemplateVers=2
+
 canonpath() {
     builtin type -t realpath.sh &>/dev/null && {
         realpath.sh -f "$@"
