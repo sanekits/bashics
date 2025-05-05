ee565b4cb62972f0420cbbdf3678c90a527ce726
LES MATHESON <LMATHESON4@bloomberg.net>
2025-05-05 11:52:56 +0000

    Sync (auto)

--- bin/install-fake.sh
+++ bin/install-fake.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # install-fake.sh
+#shellcheck disable=2154
+PS4='\033[0;33m$( _0=$?;set +e;exec 2>/dev/null;realpath -- "${BASH_SOURCE[0]:-?}:${LINENO} \033[0;35m^$_0\033[32m ${FUNCNAME[0]:-?}()=>" )\033[;0m '
 
 scriptName="${scriptName:-"$(command readlink -f -- "$0")"}"
 scriptDir="$(command dirname -- "${scriptName}")"
@@ -15,7 +17,7 @@ main() {
     set -x
     (
         cd "${HOME}/.local/bin"
-        [[ -h ./bashics ]] && die "bashics is already fake-installed: $(command ls -ald "$./bashics")"
+        [[ -h ./bashics ]] && die "bashics is already fake-installed: $(command ls -ald ./bashics)"
         [[ -d ./bashics ]] && mv ./bashics ./bashics-bak.$$
         ln -sf "${scriptDir}" ./bashics
     )
